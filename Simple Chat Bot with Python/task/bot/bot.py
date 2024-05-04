fc00032b5c161847b804ed816556f682697cda24
Eric Cheyne <eric.cheyne@gmail.com>
2024-05-04 14:48:03 -0700

    Added a while loop to read a number and count to it

--- Simple Chat Bot with Python/task/bot/bot.py
+++ Simple Chat Bot with Python/task/bot/bot.py
@@ -4,17 +4,26 @@ print('Please, remind me your name.')
 
 name = input()
 
-print('What a great name you have, ' + name + '!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
+print(f"What a great name you have, {name}!")
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3, 5 and 7.")
 
 # reading all remainders
-remainder3 = int(input()) % 3
-remainder5 = int(input()) % 5
-remainder7 = int(input()) % 7
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
 
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 
-your_age = (age)
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print("Now I will prove to you that I can count to any number you want.")
 
-print(f"Your age is {your_age}; that's a good time to start programming!")
+# read a number and count to it here
+number = int(input())
+
+counter = 0
+while counter <= number:
+    print(str(counter) + "!")
+    counter += 1
+
+print("Completed, have a nice day!")
